25cd6da0b1655712d71ce89e8898189591ccd999
Alfaima Solano <alfaimalucia@gmail.com>
2020-10-29 09:55:59 -0500

    Versión de taller

--- fertilizantes.py
+++ fertilizantes.py
@@ -1,22 +1,48 @@
-import pulp as lp
-
-m = lp.LpProblem("Fertilizacion", lp.LpMinimize)
-
-x1 = lp.LpVariable("Avicompost",0,None,cat= lp.LpContinuous)
+#-----------------
+# Modelo
+#-----------------
+m = lp.LpProblem(
+                "Fertilizacion", #Nombre 
+                 lp.LpMinimize    #Sentido     
+                 )
+#-----------------
+# Variables
+#----------------- 
+#Avicompost
+x1 = lp.LpVariable("Avicompost", #Nombre
+                   0,            #Valor mínimo
+                   None,         #Valor máximo
+                   cat= lp.LpContinuous #naturaleza
+                  )
+#TripleNPK
 x2 = lp.LpVariable("TripleNPK",0,None,cat= lp.LpContinuous)
-
-m += x1 >= 1500, "MinCompost"
-m += 0.0147*x1 + 0.18*x2 >= 150, "Nitrogeno"
-m += 0.0436*x1 + 0.18*x2 >= 90, "Fosforo"
-m += 0.0307*x1 + 0.18*x2 >= 200, "Potasio"
+                 
+#-----------------
+# Restricciones
+#----------------- 
+m += x1 >= 1.5, "MinCompost"
+m += 0.0147*x1 + 0.18*x2 >= 0.15, "Nitrogeno"
+m += 0.0436*x1 + 0.18*x2 >= 0.09, "Fosforo"
+m += 0.0307*x1 + 0.18*x2 >= 0.20, "Potasio"
+                 
+#-----------------
+# Función objetivo
+#-----------------
 
 m += 280*x1 + 1820*x2 
+                 
+#-----------------
+# Archivo de optimización
+#-----------------
 
 m.writeLP("Fertilizantes.lp")
-m.solve()
-
+m.solve(lp.PULP_CBC_CMD(msg=0))
+                 
+#-----------------
+# Solución
+#-----------------
 print("Status:", lp.LpStatus[m.status])
 
+print("Recomendación de fertilización")
 for v in m.variables():
-	print(v.name, "=", v.varValue)
-
+	print(v.name, "=", v.varValue, "kg")
